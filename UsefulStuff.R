24ac5f0b99ca0e9cc4280bc35284596bef439919
Jakob256 <75954894+Jakob256@users.noreply.github.com>
2022-04-25 08:47:26 +0200

    Update UsefulStuff.R

--- UsefulStuff.R
+++ UsefulStuff.R
@@ -1,91 +1,149 @@
-.LastInfo<<-0
+.progPar <<- as.data.frame(matrix(0,ncol=5))
+colnames(.progPar)=c("progressStartTime","lastInfo","setType","totalLength","done")
 
-.progress<- function(whereNow,setInfo1,setInfo2=NULL){
+
+
+.progressStart <- function(totalLength){
   time=round(as.numeric(Sys.time()))
-  if (time==.LastInfo){return()} ##chick escape
+  .progPar$totalLength<<-totalLength
+  .progPar$done<<- -1
+  .progPar$progressStartTime<<- time
+  .progPar$lastInfo<<- time
+  .progPar$setType<<- "blindCounter"
+  cat("Blind counter started\n\n")
+}
 
+
+.progress<- function(whereNow=NULL,setInfo1=NULL,setInfo2=NULL){
+  if (is.null(whereNow)){.progPar$done <<- .progPar$done+1} ## no input at all -> we have a blind Counter
+
+  time=round(as.numeric(Sys.time()))
+  if (time==.progPar$lastInfo){return()} ##quick escape - since anyway no update necessary
+  
+  
   ## check if the loop has just started:
   justStarted=F
-  if (is.null(setInfo2)){ ## no second info
-    if (length(setInfo1)==1){ ## only endpoint given
-      if (whereNow==1){justStarted=T}
-    } else { ##set is given
+  if (!is.null(whereNow)){ ## if is null -> blind Counter -> not necessary to check if started
+    if (is.null(setInfo2)){ ## no second info
+      if (length(setInfo1)==1){ ## only endpoint given
+        if (whereNow==1){justStarted=T}
+      } else { ##set is given
+        if (whereNow==setInfo1[1]){justStarted=T}
+      }
+    } else { ## there is a second info
       if (whereNow==setInfo1[1]){justStarted=T}
     }
-  } else { ## there is a second info
-    if (whereNow==setInfo1[1]){justStarted=T}
   }
-
+  
   
   ## initialize and store stuff if just started
+  
   if (justStarted){
-    .ProgressStartTime<<-time
-    .LastInfo<<-time
+    .progPar$progressStartTime<<-time
+    .progPar$lastInfo<<-time
     
     if (is.null(setInfo2)){ ## no second info
       if (length(setInfo1)==1){ ## only endpoint given
-        .setType<<-"singleElement"
-        .totalLength<<-setInfo1
+        .progPar$setType<<-"singleElement"
+        .progPar$totalLength<<-setInfo1
       } else {                  ## set given
-        .setType<<-"set"
-        .totalLength<<-length(setInfo1)
+        .progPar$setType<<-"set"
+        .progPar$totalLength<<-length(setInfo1)
       }
     } else {
-      .setType<<-"doubleElement"
-      .totalLength<<- setInfo2-setInfo1+1
+      .progPar$setType<<-"doubleElement"
+      .progPar$totalLength<<- setInfo2-setInfo1+1
     }
+    
     cat("progress update started\n\n")
   }
   
   
-  if (time>.LastInfo){##one sec has passed; lets give an update
+  
+  if (time>.progPar$lastInfo){##one sec has passed; lets give an update
     ## calculate how many iterations done:
-    if (.setType=="singleElement"){
-      done=whereNow-1 
-    } else if (.setType=="doubleElement"){
+    
+    if (.progPar$setType=="singleElement"){
+      done=whereNow-1
+    } else if (.progPar$setType=="doubleElement"){
       done=whereNow-setInfo1
-    } else if (.setType=="set"){
-      done=which(whereNow==setInfo1)
+    } else if (.progPar$setType=="set"){
+      done=which(whereNow==setInfo1)-1
+    } else if (.progPar$setType=="blindCounter"){
+      done=.progPar$done
     }
-    remaining=.totalLength-done
     
+    remaining=.progPar$totalLength-done
     
-    remainingTime=(.totalLength-done)*(time-.ProgressStartTime)/done
+    remainingTime=(.progPar$totalLength-done)*(time-.progPar$progressStartTime)/done
     #  nrStillRemaining   *   passedTime  /  how many already
+    
     remainingTime=round(remainingTime)
+    
+    ## calculate sec, min and h
+    
+    remainingSec=remainingTime%%60
+    remainingMin=floor((remainingTime%%3600)/60)
+    remainingH=floor(remainingTime/3600)
+    
+    # make 4:3 min -> 4:03 min
+    if (remainingTime>=60){remainingSec=paste(rep("0",2-nchar(remainingSec)),remainingSec,sep="")}
+    if (remainingTime>=3600){remainingMin=paste(rep("0",2-nchar(remainingMin)),remainingMin,sep="")}
     if (remainingTime<60){
-      cat("Remaining:",remainingTime,"secs;  (",done,"/",.totalLength,")\n")
+      cat("Remaining: ",remainingSec," sec;  (",done,"/",.progPar$totalLength,")\n",sep="")
+    } else if (remainingTime<3600){
+      cat("Remaining: ",remainingMin,":",remainingSec," min;  (",done,"/",.progPar$totalLength,")\n",sep="")
     } else {
-      cat("Remaining:",floor(remainingTime/60),":",remainingTime%%60,"min;  (",done,"/",.totalLength,")\n")
+      cat("Remaining: ",remainingH,":",remainingMin,":",remainingSec," h;  (",done,"/",.progPar$totalLength,")\n",sep="")
     }
-    .LastInfo<<-time
+    
+    .progPar$lastInfo<<-time
   }
 }
 
 
 
 
-for (i in 1:300){
-  .progress(i,300)
-  Sys.sleep(1)
-  ## stuff here
+if(FALSE){
+  f <- function(){Sys.sleep(0.1)}
+  g <- function(){Sys.sleep(1)}
+  h <- function(){Sys.sleep(2)}
+  
+  for (i in 1:362){
+    .progress(i,362)
+    ## stuff here
+    g()
+  }
+  
+  for (i in 101:400){
+    .progress(i,101,400)
+    ## stuff here
+    f()
+  }
+  
+  for (n in LETTERS){
+    .progress(n,LETTERS)
+    f()
+    ## stuff here
+  }
+  
+  .progressStart(20*21/2)
+  for (i in 1:20){
+    for (j in i:20){
+      .progress()
+      g()
+    }
+  }
 }
 
-for (i in 1:300){
-  .progress(i,1,300)
-  Sys.sleep(1)
-  ## stuff here
-}
 
-for (n in LETTERS){
-  .progress(n,LETTERS)
-  Sys.sleep(1)
-  ## stuff here
-}
+# you may pass
 
-# you might pass 
 #1) the set
 #2) the endpoint of the set (assuming start=1)
 #3) the start and endpoint of the set
+#4) nothing at all
+
+
 
 # simply pass it in chronological order
